357a11b6e506b6916751a72d0425f332ae6de8fe
brian <brian.p.coons@gmail.com>
2020-02-04 14:17:46 -0700

    fixed some props errors

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -24,15 +24,15 @@ const NavBar = props => {
         console.log('Name: ' + profile.getName());
         console.log('Image URL: ' + profile.getImageUrl());
         console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-        this.props.handleSuccessfulLogin()
-        this.props.setUser(profile.getName())
+        props.handleSuccessfulLogin()
+        props.setUser(profile.getName())
     }
 
     const handleSignOut = () => {
         var auth2 = gapi.auth2.getAuthInstance();
         auth2.signOut().then(function () {
             console.log('User signed out.');
-            this.props.handleSuccessfulLogout()
+            props.handleSuccessfulLogout()
         });
     }
 
