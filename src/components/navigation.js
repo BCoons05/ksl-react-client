49941892b89887467e12d5c6ddf2121f6e8fd73c
brian <brian.p.coons@gmail.com>
2020-02-04 15:28:59 -0700

    new google strategy

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,6 +1,7 @@
 import React, {Component, useEffect, useState} from 'react'
 import axios from "axios";
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import GoogleLogin from 'react-google-login';
 
 import { withRouter } from 'react-router';
 import { NavLink } from 'react-router-dom';
@@ -18,34 +19,39 @@ export default class NavBar extends Component {
     // }
 
 
-    onSignIn = (googleUser) => {
-        var profile = user.getBasicProfile();
-        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-        console.log('Name: ' + profile.getName());
-        console.log('Image URL: ' + profile.getImageUrl());
-        console.log('Email: ' + profile.getEmail());
+    // onSignIn = (googleUser) => {
+    //     var profile = user.getBasicProfile();
+    //     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    //     console.log('Name: ' + profile.getName());
+    //     console.log('Image URL: ' + profile.getImageUrl());
+    //     console.log('Email: ' + profile.getEmail());
 
-        // gapi.load('auth2', function() {
-        //     auth2 = gapi.auth2.init({
-        //       client_id: 'CLIENT_ID.apps.googleusercontent.com',
-        //       fetch_basic_profile: false,
-        //       scope: 'profile'
-        //     });
+    //     // gapi.load('auth2', function() {
+    //     //     auth2 = gapi.auth2.init({
+    //     //       client_id: 'CLIENT_ID.apps.googleusercontent.com',
+    //     //       fetch_basic_profile: false,
+    //     //       scope: 'profile'
+    //     //     });
 
-        //     auth2.signIn().then(function() {
-        //         console.log(auth2.currentUser.get().getId());
-        //       });
-        //     });
+    //     //     auth2.signIn().then(function() {
+    //     //         console.log(auth2.currentUser.get().getId());
+    //     //       });
+    //     //     });
 
-        // if (auth2.isSignedIn.get()) {
-        //     var profile = auth2.currentUser.get().getBasicProfile();
-        //     console.log('ID: ' + profile.getId());
-        //     console.log('Full Name: ' + profile.getName());
-        //     console.log('Email: ' + profile.getEmail());
-            this.props.handleSuccessfulLogin(profile.getName())
-        // } else {
-        //       console.log("can't find user info")
-        // }
+    //     // if (auth2.isSignedIn.get()) {
+    //     //     var profile = auth2.currentUser.get().getBasicProfile();
+    //     //     console.log('ID: ' + profile.getId());
+    //     //     console.log('Full Name: ' + profile.getName());
+    //     //     console.log('Email: ' + profile.getEmail());
+    //         this.props.handleSuccessfulLogin(profile.getName())
+    //     // } else {
+    //     //       console.log("can't find user info")
+    //     // }
+    // }
+
+    responseGoogle = response => {
+        let userName = response.profileObj.name
+        this.props.handleSuccessfulLogin(userName)
     }
 
     handleSignOut = () => {
@@ -55,17 +61,7 @@ export default class NavBar extends Component {
             this.props.handleSuccessfulLogout()
         });
     }
-
     
-    componentDidMount() {
-        window.gapi.load('auth2', () => {
-            this.auth2 = gapi.auth2.init({
-            client_id: '866689668072-3ov7is4028nben3tloj0lh1emoab3me5.apps.googleusercontent.com',
-            })
-        })
-        }
-    
-
     render(){
         return (
             <div className="nav-wrapper">
@@ -90,7 +86,13 @@ export default class NavBar extends Component {
                 <div className="right-side">
                     {this.props.loggedInStatus === "LOGGED_IN" ? <a onClick={handleSignOut}>
                         Sign Out
-                    </a> : <div class="g-signin2" data-onsuccess={"onSignIn"}></div>}
+                    </a> : <GoogleLogin
+                                clientId="866689668072-3ov7is4028nben3tloj0lh1emoab3me5.apps.googleusercontent.com"
+                                buttonText="Login"
+                                onSuccess={responseGoogle}
+                                onFailure={responseGoogle}
+                                cookiePolicy={'single_host_origin'}
+                            />}
                 </div>
             </div>
         )
