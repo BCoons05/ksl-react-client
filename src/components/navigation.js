ccf86db76e45560caedfb04bc0b8a4c5856e80e5
brian <brian.p.coons@gmail.com>
2020-02-04 13:02:11 -0700

    realy this time

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -18,7 +18,7 @@ const NavigationComponent = props => {
     }
 
 
-    onSignIn = (googleUser) => {
+    const onSignIn = (googleUser) => {
         var profile = googleUser.getBasicProfile();
         console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
         console.log('Name: ' + profile.getName());
