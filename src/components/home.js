6cd6992e3a09b647a43e2d351a5174b84663ac03
brian <brian.p.coons@gmail.com>
2020-02-06 09:01:40 -0700

    updated styles

--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,25 @@
-import React from "react"
+import React, { Component } from "react"
 
-export default function() {
-    return (
-        <div>
-            Home Page
-        </div>
-    )
+export default class Home extends Component {
+    constructor(props){
+        super(props)
+
+        this.state ={
+            setVisible: false
+        }
+    }
+
+    render(){
+        return (
+            <div className="home-page-wrapper">
+                <div className="dev-info-container" onClick={() => this.setVisible()}>
+                    {`This site was built as my solo Capstone project at Bottega. 
+                    To learn more about the site and technologies used to build it, 
+                    click anywhere in this area.
+                    
+                    Otherwise, click search to start searching or to sign in.`}
+                </div>
+            </div>
+        )
+    }
 }
