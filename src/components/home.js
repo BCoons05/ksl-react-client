63c4b614577982726ff44a6b24ca4f402849d710
brian <brian.p.coons@gmail.com>
2020-07-14 11:43:27 -0600

    added dependencies...

--- src/components/home.js
+++ src/components/home.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react"
+const cheerio = require('cheerio');
+const request = require('request');
 
 export default class Home extends Component {
     constructor(props){
